80c41442ebfa94041f8f44dacd4c59f4c58dd5da
Lan Hikari <lanzaav5@gmail.com>
2019-03-31 11:54:19 -0400

    added so many things

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ import player1
 
 pygame.init()
 
-def draw_player(screen, bg, x, y):
-    pygame.draw.rect(screen, (0, 0, 0), (x, y, 40, 60))
+def draw_player(screen, bg, x, y, height, width):
+    pygame.draw.rect(screen, (0, 0, 0), (x, y, height, width))
     pygame.display.update()
     screen.blit(bg, [0, 0])
 def main():
     screen = pygame.display.set_mode((500, 500))
     x=50
     y=50
+
+    pHeight = 60
+    pWidth = 40
+
     speed = 5
     bg = pygame.image.load("grass.png")
     sword = pygame.image.load("sword.png")
@@ -18,10 +22,19 @@ def main():
     running = True
     screen.blit(bg, [0,0])
     p_dir = 'r'
+    player = pygame.Rect(x, y, pHeight, pWidth)
+
+    #draw text
+    pygame.font.init()
+    myfont = pygame.font.SysFont('Comic Sans MS', 15)
+
     # main loop
     while running:
         pygame.time.delay(25)
-        draw_player(screen, bg, x, y)
+        coord = str(player.x) + "/"+ str(player.y)
+        textsurface = myfont.render(coord, False, (0, 0, 0))
+        screen.blit(textsurface,(0,0))
+        draw_player(screen, bg, player.x, player.y, player.height, player.width)
 
         for event in pygame.event.get():
             # only do something if the event is of type QUIT
@@ -32,36 +45,32 @@ def main():
         key = pygame.key.get_pressed()
 
         if key[pygame.K_RIGHT] and x<460:
-            x=x+speed
-            #screen.blit(bg, [0, 0])
-            pygame.draw.rect(screen, (0, 0, 0), (x,y, 40, 60))
+            player.x=player.x+speed
+            #draw_player(screen, bg, player.x, player.y, player.height, player.width)
+
             p_dir = 'r'
         if key[pygame.K_LEFT] and x>0:
-            x=x-speed
-            screen.blit(bg, [0, 0])
-            pygame.draw.rect(screen, (0, 0, 0), (x, y, 40, 60))
+            player.x=player.x-speed
+            #draw_player(screen, bg, player.x, player.y, player.height, player.width)
             p_dir = 'l'
         if key[pygame.K_UP] and y>0:
-            y=y-speed
-            #screen.blit(bg, [0, 0])
-            pygame.draw.rect(screen, (0, 0, 0), (x, y, 40, 60))
+            player.y=player.y-speed
+            #draw_player(screen, bg, player.x, player.y, player.height, player.width)
             p_dir = 'u'
         if key[pygame.K_DOWN] and y<440:
-            y=y+speed
-            #screen.blit(bg, [0, 0])
+            player.y=player.y+speed
+            #draw_player(screen, bg, player.x, player.y, player.height, player.width)
             p_dir = 'd'
-            pygame.draw.rect(screen, (0, 0, 0), (x, y, 40, 60))
         if key[pygame.K_SPACE]:
-            #screen.blit(bg, [0, 0])
-
             if p_dir == 'r':
-                screen.blit(sword, [x+50, y+15])
+                screen.blit(sword, [player.x+50, player.y+15])
             if p_dir == 'l':
-                screen.blit(pygame.transform.rotate(sword, 180), [x-55, y+20])
+                screen.blit(pygame.transform.rotate(sword, 180), [player.x-55, player.y+20])
             if p_dir == 'u':
-                screen.blit(pygame.transform.rotate(sword, 90), [x, y-55])
+                screen.blit(pygame.transform.rotate(sword, 90), [player.x, player.y-55])
             if p_dir == 'd':
-                screen.blit(pygame.transform.rotate(sword, 270), [x, y+65])
+                screen.blit(pygame.transform.rotate(sword, 270), [player.x, player.y+65])
+
 
 
 main()
